d8ba7bac1e23a5d133de2877b9a1fd613709ac13
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2021-08-08 13:30:16 +0530

    inner txt error

--- script.js
+++ script.js
@@ -1,70 +1,4 @@
-<<<<<<< HEAD
-const billDiv = document.querySelector(".billEnter");
-const billAmt=document.querySelector("#billAmt");
-const nextBtn = document.querySelector(".next");
-
-
-
-const cashDiv = document.querySelector(".cashEnter");
-const cashGiven=document.querySelector("#amtGiven")
-const chk=document.querySelector("#check")
-const message=document.querySelector("#message")
-
-
-const outputDis=document.querySelector(".notesDisplay")
-
-const notes=[2000, 500, 100, 20, 10, 5, 1];
-
-
-chk.addEventListener("click", function validatingCashAndBillamt() {
-    hidemessage();
-    if (Number(cashGiven.value) < Number(billAmt.value)) {
-      showMessage("The cash amount should be atleast equal to bill amount");
-    }
-    else{
-        const amountReturn = Number(cashGiven.value) - Number(billAmt.value);
-        calculateChange(amountReturn);
-    }
-  });
-
-//calculating the changes
-
-function calculateChange(amountReturn){
-    for(let i=0;i<notes.length;i++){
-        const no_of_notes=Math.trunc(amountReturn/notes[i]);
-        amountReturn%=notes[i];
-        outputDis[i].innerText=no_of_notes;
-    }
-    console.log(no_of_notes)
-}
-
-
-// hidding the error message at first
-function hidemessage(){
-    message.style.display="none"
-}
-
-
-// error message display function
-function showMessage(msg){
-    message.style.display="block";
-    message.innerText=msg;
-}
-
-
-cashDiv.style.display="none";
-nextBtn.addEventListener('click', ()=>{
-    hidemessage();
-    if(Number(billAmt.value)>0){
-        nextBtn.style.display = "none";
-        cashDiv.style.display = "block";
-    }
-    else{
-        showMessage("Enter valid bill amount");
-    }
-} )
-=======
-const billDiv = document.querySelector(".billEnter");
+// const billDiv = document.querySelector(".billEnter");
 const billAmt = document.querySelector("#billAmt");
 const nextBtn = document.querySelector(".next");
 
@@ -90,7 +24,7 @@ chk.addEventListener("click", function validatingCashAndBillamt() {
     const amountReturn = Number(cashGiven.value) - Number(billAmt.value);
     console.log("remaining amount: ", billAmt.value - cashGiven.value);
     console.log(amountReturn);
-    //calculateChange(amountReturn);
+    calculateChange(amountReturn);
   
 });
 
@@ -115,6 +49,8 @@ function showMessage(msg) {
   message.innerText = msg;
 }
 
+
+// next button hide
 cashDiv.style.display = "none";
 nextBtn.addEventListener("click", () => {
   hidemessage();
@@ -125,4 +61,4 @@ nextBtn.addEventListener("click", () => {
     showMessage("Enter valid bill amount");
   }
 });
->>>>>>> 927f7e481c9dc0eb28e117da8a9edfff1f5658da
+
