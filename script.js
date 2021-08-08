b6a25a7977ea702f2fbee23a06d3c5d112840f91
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2021-08-08 12:42:21 +0530

    problem with check button

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
+const billDiv = document.querySelector(".billEnter");
 const billAmt=document.querySelector("#billAmt");
+const nextBtn = document.querySelector(".next");
+
+
+
+const cashDiv = document.querySelector(".cashEnter");
 const cashGiven=document.querySelector("#amtGiven")
 const chk=document.querySelector("#check")
-const outputDis=document.querySelector(".notesDisplay")
 const message=document.querySelector("#message")
 
+
+const outputDis=document.querySelector(".notesDisplay")
+
 const notes=[2000, 500, 100, 20, 10, 5, 1];
+
+
 chk.addEventListener("click", function validatingCashAndBillamt() {
     hidemessage();
-    if(billAmt.value>0){
-        if(cashGiven.value>=billAmt.value){
-            const amountReturn=cashGiven.value-billAmt.value;
-            calculateChange(amountReturn);
-        }
-        else{
-            showMessage("The cash amount should be atleast equal to bill amount");
-        }
+    if(billAmt>0 && cashGiven.value>=billAmt.value){
+        const amountReturn=cashGiven.value-billAmt.value;
+        calculateChange(amountReturn);
     }
     else{
-        showMessage("invalid amount")
+        showMessage("The cash amount should be atleast equal to bill amount");
     }
 });
 
 //calculating the changes
 
-function calculateChange(amountReturn){
+function calculateChange(amtReturn){
     for(let i=0;i<notes.length;i++){
-        const no_of_notes=Math.trunc(amountReturn/notes[i]);
-        amountReturn%=notes[i];
+        const no_of_notes=Math.trunc(amtReturn/notes[i]);
+        amtReturn%=notes[i];
         outputDis[i].innerText=no_of_notes;
     }
 }
+
 
 // hidding the error message at first
 function hidemessage(){
@@ -43,3 +49,15 @@ function showMessage(msg){
     message.innerText=msg;
 }
 
+
+cashDiv.style.display="none";
+nextBtn.addEventListener('click', ()=>{
+    hidemessage();
+    if(Number(billAmt.value)>0){
+        nextBtn.style.display = "none";
+        cashDiv.style.display = "block";
+    }
+    else{
+        showMessage("Enter valid bill amount");
+    }
+} )
