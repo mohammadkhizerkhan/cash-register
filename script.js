2d0e379f80997f8b5158c848a6646f8cdeae17e4
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2021-08-08 14:16:29 +0530

    done with logic part

--- script.js
+++ script.js
@@ -7,35 +7,33 @@ const cashGiven = document.querySelector("#amtGiven");
 const chk = document.querySelector("#check");
 const message = document.querySelector("#message");
 
-const outputDis = document.querySelector(".notesDisplay");
+const outputDiv = document.querySelector(".outputEnter");
+const outputDis = document.querySelectorAll(".notesDisplay");
 
 const notes = [2000, 500, 100, 20, 10, 5, 1];
 
 chk.addEventListener("click", function validatingCashAndBillamt() {
   hidemessage();
-  console.log(billAmt.value);
-  console.log(cashGiven.value);
-  if (Number(billAmt.value) === 0) {
-    console.log("Amount can be zero");
-  }
+  outputDiv.style.display = "block";
   if (Number(cashGiven.value) < Number(billAmt.value)) {
     showMessage("The cash amount should be atleast equal to bill amount");
   }
-    const amountReturn = Number(cashGiven.value) - Number(billAmt.value);
-    console.log("remaining amount: ", billAmt.value - cashGiven.value);
-    console.log(amountReturn);
-    calculateChange(amountReturn);
+  else{
+      const amountReturn = Number(cashGiven.value) - Number(billAmt.value);
+      calculateChange(amountReturn);
+  }
   
 });
 
 //calculating the changes
 
-function calculateChange(amtReturn) {
+function calculateChange(amountReturn) {
   for (let i = 0; i < notes.length; i++) {
-    const no_of_notes = Math.trunc(amtReturn / notes[i]);
-    amtReturn %= notes[i];
+    const no_of_notes = Math.trunc(amountReturn / notes[i]);
+    amountReturn %= notes[i];
     outputDis[i].innerText = no_of_notes;
   }
+  console.log(no_of_notes)
 }
 
 // hidding the error message at first
@@ -51,6 +49,8 @@ function showMessage(msg) {
 
 
 // next button hide
+
+outputDiv.style.display = "none";
 cashDiv.style.display = "none";
 nextBtn.addEventListener("click", () => {
   hidemessage();
